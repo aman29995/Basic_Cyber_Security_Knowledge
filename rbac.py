f126114e24838e360eaea33f7624d6dde73dbdc0
aman29995 <aman29995@gmail.com>
2026-01-05 12:28:11 +0530

    rbac.py

--- rbac.py
+++ rbac.py
@@ -1,12 +1,12 @@
-
 import streamlit as st
 
 def select_role():
     if "role" not in st.session_state:
         st.session_state.role = None
+
     if not st.session_state.role:
         role = st.radio("Select Role", ["SOC Analyst", "VM Lead"])
         if st.button("Continue"):
             st.session_state.role = role
-            st.experimental_rerun()
+            st.rerun()   # ✅ FIXED
         st.stop()
